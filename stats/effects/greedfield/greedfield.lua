f1246281b3fd97e923c6ae39930e3f8a36c5d80f
robotguy4 <robotguy4@gmail.com>
2016-12-17 12:51:17 -1000

    Initial

---
+++ stats/effects/greedfield/greedfield.lua
@@ -0,0 +1,26 @@
+local theentity
+
+function init()
+	script.setUpdateDelta(5)
+		self.applyDamageRequest =0
+
+end
+
+function tablelength(T)
+  local count = 0
+  for _ in pairs(T) do count = count + 1 end
+  return count
+end
+
+function update(dt)
+	theentity = effect.sourceEntity()
+--	sb.logInfo("assf")
+--	sb.logInfo("theentity id#: " .. theentity)
+	thepos = world.entityPosition(theentity)
+--	sb.logInfo("theposi      : " .. thepos[1].. ", " .. thepos[2])
+	theitems = world.itemDropQuery(thepos,15.0,{boundMode="position"})
+--	sb.logInfo("ssfdf" .. table.tostring(theitems) .. "  " .. tablelength(theitems))
+	if tablelength(theitems) > 0 then
+		world.spawnProjectile("greedfield",mcontroller.position(),entity.id(),{0,0},true,configBombDrop);
+	end
+end
